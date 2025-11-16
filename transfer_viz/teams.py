22a952a61cc4a57ef6f17841205089c5cd62135e
Umesh Timalsina <umesh.timalsina@ioe.edu.np>
2025-11-16 08:03:31 -0600

    Visualize maps using folium

--- transfer_viz/teams.py
+++ transfer_viz/teams.py
@@ -1,5 +1,8 @@
 import cfbd
 import pandas as pd
+import folium
+
+from transfer_viz.utils import load_dataset
 
 
 class TeamsParser:
@@ -16,7 +19,7 @@ class TeamsParser:
             pd.DataFrame: DataFrame containing team information.
         """
         try:
-            teams = self.api_instance.get_fbs_teams()
+            teams = self.api_instance.get_teams()
             teams_data = []
             for team in teams:
                 team_info = {
@@ -65,7 +68,39 @@ class TeamsParser:
 def parse_teams(url: str, bearer_token: str) -> pd.DataFrame:
     """Parse and return the list of teams from the API."""
     parser = TeamsParser(url=url, bearer_token=bearer_token)
+
     return parser.parse()
+
+
+def filter_teams() -> pd.DataFrame:
+    """Filter teams details based on team metadata to save processed teams dataset."""
+    teams_details_df = load_dataset("raw_teams_details")
+    team_metadata_df = load_dataset("team_metadata")
+    filtered_details_df = teams_details_df[
+        teams_details_df["id"].isin(team_metadata_df["TeamId"])
+    ]
+
+    return filtered_details_df
+
+
+def teams_map():
+    teams_details_df = load_dataset("teams_details")
+    map_center = [39.8283, -98.5795]  # Center of the US
+    teams_map = folium.Map(location=map_center, zoom_start=4)
+
+    for _, row in teams_details_df.iterrows():
+        if pd.notnull(row["location_latitude"]) and pd.notnull(
+            row["location_longitude"]
+        ):
+            folium.Marker(
+                location=[row["location_latitude"], row["location_longitude"]],
+                popup=f"{row['school']} ({row['abbreviation']})",
+                icon=folium.Icon(
+                    color="white", icon="info-sign", icon_color=row["color"]
+                ),
+            ).add_to(teams_map)
+
+    return teams_map
 
 
 def run(args=None):
@@ -92,19 +127,47 @@ def run(args=None):
         help="Access token for the College Football Data API",
     )
 
+    parser.add_argument(
+        "command",
+        metavar="COMMAND",
+        help=", ".join(sorted(["visualize", "filter", "parse", "teams_map"])),
+    )
+
     parsed_args = parser.parse_args(args)
 
-    teams_df = parse_teams(
-        url=parsed_args.api_url, bearer_token=parsed_args.access_token
-    )
+    if parsed_args.command == "visualize":
+        print("Visualization functionality is not implemented yet.")
+    elif parsed_args.command == "filter":
+        filtered_teams_df = filter_teams()
 
-    dataset_path = (
-        Path(__file__).parent.parent
-        / "data"
-        / "raw"
-        / "team_info"
-        / "teams_details.csv"
-    )
-    dataset_path.parent.mkdir(parents=True, exist_ok=True)
-    teams_df.to_csv(dataset_path, index=False)
-    print(f"Teams data saved to {dataset_path}")
+        dataset_path = (
+            Path(__file__).parent.parent / "data" / "processed" / "teams_details.csv"
+        )
+        dataset_path.parent.mkdir(parents=True, exist_ok=True)
+        filtered_teams_df.to_csv(dataset_path, index=False)
+        print(f"Filtered teams data saved to {dataset_path}")
+
+    elif parsed_args.command == "teams_map":
+        teams_map_obj = teams_map()
+        map_path = (
+            Path(__file__).parent.parent / "data" / "processed" / "teams_map.html"
+        )
+        map_path.parent.mkdir(parents=True, exist_ok=True)
+        teams_map_obj.save(str(map_path))
+        print(f"Teams map saved to {map_path}")
+
+    elif parsed_args.command == "parse":
+        teams_df = parse_teams(
+            url=parsed_args.api_url, bearer_token=parsed_args.access_token
+        )
+
+        dataset_path = (
+            Path(__file__).parent.parent
+            / "data"
+            / "raw"
+            / "team_info"
+            / "raw_teams_details.csv"
+        )
+        dataset_path.parent.mkdir(parents=True, exist_ok=True)
+        teams_df.to_csv(dataset_path, index=False)
+        print(f"Teams data saved to {dataset_path}")
---
+++ transfer_viz/utils.py
@@ -0,0 +1,48 @@
+from typing import Literal
+import pandas as pd
+from pathlib import Path
+
+
+def load_dataset(
+    name: Literal[
+        "team_metadata",
+        "transfers_data",
+        "teams_details",
+        "raw_teams_details",
+        "2022_FPI",
+        "2023_FPI",
+        "2024_FPI",
+        "2022_team_records",
+        "2023_team_records",
+        "2024_team_records",
+        "coaches",
+    ],
+) -> pd.DataFrame:
+    """Load dataset by name from the data/raw directory."""
+    raw_processed_dir = {
+        "team_metadata": "processed",
+        "transfers_data": "processed",
+        "teams_details": "processed",
+        "raw_teams_details": "raw/team_info",
+        "2022_FPI": "raw/team_info",
+        "2023_FPI": "raw/team_info",
+        "2024_FPI": "raw/team_info",
+        "2022_team_records": "raw/team_info",
+        "2023_team_records": "raw/team_info",
+        "2024_team_records": "raw/team_info",
+        "coaches": "raw/team_info",
+        "2022_transfers": "raw/transfer_portal_info",
+        "2023_transfers": "raw/transfer_portal_info",
+        "2024_transfers": "raw/transfer_portal_info",
+    }
+
+    location = raw_processed_dir.get(name, "raw")
+
+    dataset_path = Path(__file__).parent.parent / "data" / location / f"{name}.csv"
+    try:
+        dataset = pd.read_csv(dataset_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"Dataset {name} not found at {dataset_path}. Please ensure the file exists and run any necessary data preparation scripts."
+        )
+    return dataset
